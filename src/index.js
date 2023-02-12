4ef724667c2f674b032fe26ecc528ee943697676
hungppi <hungppp303@gmail.com>
2023-02-12 14:47:59 +0700

    test husky

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
-const {
-  engine
-} = require('express-handlebars');
+const { engine } = require('express-handlebars');
 const app = express();
 const route = require('./routes/index');
 app.use(morgan('combined'));
@@ -12,10 +10,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // template engine
 app.engine(
-  'hbs',
-  engine({
-    extname: '.hbs',
-  }),
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
 );
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
@@ -25,15 +23,16 @@ route(app);
 const port = 3000;
 
 app.listen(port, () =>
-  console.log(`app listening at http://localhost:${port}`),
+    console.log(`app listening at http://localhost:${port}`),
 );
 
 const abc = {
-  a: 1,
-  b: 2,
-  c: 3,
-  d: 'dd',
-  dfsdj: 'fdsklf',
-  "dkjsl": 'dsfkjkl',
-  'fdskj': "fsldkfjlk"
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 'dd',
+    dfsdj: 'fdsklf',
+    dkjsl: 'dsfkjkl',
+    fdskj: 'fsldkfjlk',
+    djslfkj: 'fdskljf',
 };
