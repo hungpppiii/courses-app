ccf1b34dee1ee159ed53ecb42cb324c586c93eac
hungpppiii <hungppp303@gmail.com>
2023-04-08 01:05:20 +0700

    connect mongoDB Atlas

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
+        const name = process.env.NAME;
+        const password = process.env.PASSWORD;
         await mongoose
-            .connect('mongodb://localhost:27017/learnJs')
+            .connect(
+                `mongodb+srv://${name}:${password}@cluster0.rhsnast.mongodb.net/?retryWrites=true&w=majority`,
+            )
             .then(() => console.log('Connected!'));
         console.log('db connect successfully!');
     } catch (error) {
